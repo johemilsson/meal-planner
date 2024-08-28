c7624dd29d2c9c34978a3ea666bfa8e3b78ce2f7
Johannes Emilsson <johannes.emilsson@gmail.com>
2024-08-28 22:25:55 +0200

    Updated Reciple class and added planner file with initial ideas

---
+++ src/planner.py
@@ -0,0 +1,7 @@
+"""
+1. Get all recipies from directory
+2. Assign recipe to each day of the week
+3. Consolidate all ingredients
+4. Upload ingredients to google keep (using gkeepapi)
+5. Print pdf with recipies and day
+"""
--- src/recipe.py
+++ src/recipe.py
@@ -8,6 +8,23 @@ class Recipe:
         self.ingredients: set = None
         self.instructions: list = None
 
+
+    def load(self, src):
+        with open(src, 'r') as fid:
+            dct = yaml.safe_load(fid)
+
+        self.name = dct["name"]
+        self.ingredients = dct["ingredients"]
+        self.instructions = dct["instructions"]
+
+    def get_ingredients(self):
+        ingredients_list = set()
+        for dct in self.ingredients:
+            ingredients_list.add(list(dct.keys())[0])
+
+        return ingredients_list
+
+
     def save(self, filename: str, directory=None):
         save_dct = {
             "name": self.name,
@@ -23,7 +40,5 @@ class Recipe:
 
 if __name__ == "__main__":
     recipe = Recipe()
-    recipe.name = "Test"
-    recipe.ingredients = set([("Agg","5"), ("Mjol", "3dl"), ("Socker", "3dl")])
-    recipe.instructions = ["Blanda", "Servera", "Njut"]
-    recipe.save("test.yml")
+    recipe.load("/home/johannes/Projects/Python/meal-planner/src/recipies/test.yml")
+    ingredients = recipe.get_ingredients()
