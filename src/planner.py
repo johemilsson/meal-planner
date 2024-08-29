b519e4dfffeea1b3d381a7d5b81ef5de2a9b8509
Johannes Emilsson <johannes.emilsson@gmail.com>
2024-08-29 21:53:34 +0200

    Added Planner class and first method to get all recipies

--- src/planner.py
+++ src/planner.py
@@ -4,4 +4,28 @@
 3. Consolidate all ingredients
 4. Upload ingredients to google keep (using gkeepapi)
 5. Print pdf with recipies and day
+
+import random
+
+random.sample(list, n) # Select n unique items from list
 """
+import os
+
+class Planner:
+    def __init__(self):
+        self.recipies = self._get_recipies()
+
+    @staticmethod
+    def _get_recipies():
+        recipies_dir = os.path.join(
+            os.path.dirname(__file__),
+            "recipies",
+        )
+        recipies = [
+            recipe for recipe in os.listdir(recipies_dir) if recipe.endswith(".yml")
+        ]
+
+        return recipies
+
+if __name__ == "__main__":
+    planner = Planner()
